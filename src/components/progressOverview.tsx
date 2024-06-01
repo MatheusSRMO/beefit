cb1604f8d4cc77a49300bd09d55a094172ea0c44
MatheusSRMO <matheus.souzaribeiro@gmail.com>
2024-06-01 19:58:31 -0300

    feat: Add ProgressOverview component
    This commit adds a new component called ProgressOverview. It is a component that displays an overview of the user's progress with a progress bar. The purpose of this commit is to provide a visually appealing way to showcase the user's progress in the application.

---
+++ src/components/progressOverview.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import ProgressBar from './progressBar'
+
+interface ProgressOverviewProps {
+  progress: number
+}
+
+export default function ProgressOverview({ progress }: ProgressOverviewProps) {
+  return (
+    <View className='bg-[#080835] p-3 mt-10 w-[90%] rounded-full'>
+      <Text className="text-[#FFDC98] text-2xl font-bold -top-10">Seus Rendimentos</Text>
+
+      {/* ProgressBar */}
+      <ProgressBar progress={progress} className='-top-4' />
+    </View>
+  )
+}
