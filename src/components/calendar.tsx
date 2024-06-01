5ff95b920fb7e220d9db6bf65e6aaec7e0b525ee
MatheusSRMO <matheus.souzaribeiro@gmail.com>
2024-06-01 19:58:49 -0300

    feat: Add Calendar component
    This commit adds a new component called Calendar. It is a component that displays a calendar using the react-native-calendars library. The purpose of this commit is to provide a convenient way to select dates and display them with custom styling.

---
+++ src/components/calendar.tsx
@@ -0,0 +1,142 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { LocaleConfig, Calendar as RNCalendar } from 'react-native-calendars';
+import { Theme } from 'react-native-calendars/src/types';
+
+LocaleConfig.locales['pt-br'] = {
+  monthNames: [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro'
+  ],
+  monthNamesShort: [
+    'Jan',
+    'Fev',
+    'Mar',
+    'Abr',
+    'Mai',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Set',
+    'Out',
+    'Nov',
+    'Dez'
+  ],
+  dayNames: [
+    'Domingo',
+    'Segunda',
+    'Terça',
+    'Quarta',
+    'Quinta',
+    'Sexta',
+    'Sábado'
+  ],
+  dayNamesShort: [
+    'D',
+    'S',
+    'T',
+    'Q',
+    'Q',
+    'S',
+    'S'
+  ],
+  today: "Hoje"
+};
+
+LocaleConfig.defaultLocale = 'pt-br';
+
+
+export default function Calendar() {
+  const [selectedDate, setSelectedDate] = useState(['']);
+
+  const onDayPress = (day: { dateString: string; }) => {
+    // Aqui é onde você vai fazer o que quiser com a data selecionada, tu vai puxar as datas da api e colocar no array selectedDate 🙂
+    setSelectedDate(prev => [...prev, day.dateString]);
+  };
+
+  return (
+    <RNCalendar
+      onDayPress={onDayPress}
+      markingType="custom"
+      markedDates={{
+        ...selectedDate.reduce((acc, date) => ({ ...acc, [date]: { selected: true, marked: false, selectedColor: '#080835' } }), {})
+      }}
+      theme={{
+        backgroundColor: '#5693b6',
+        calendarBackground: '#5693b6',
+        textSectionTitleColor: 'white',
+        selectedDayBackgroundColor: '#080835',
+        selectedDayTextColor: '#fff',
+        todayTextColor: '#FFDC98',
+        dayTextColor: 'white',
+        textDisabledColor: '#d9e1e8',
+        dotColor: '#00adf5',
+        selectedDotColor: '#74D3E7',
+        arrowColor: '#FFDC98',
+        monthTextColor: 'white',
+        textDayFontFamily: 'monospace',
+        textMonthFontFamily: 'monospace',
+        textDayHeaderFontFamily: 'monospace',
+        textDayFontWeight: 'bold',
+        textMonthFontWeight: 'bold',
+        textDayHeaderFontWeight: 'bold',
+        textMonthFontSize: 18,
+        textDayHeaderFontSize: 19,
+        'stylesheet.calendar.header': {
+          week: {
+            paddingTop: 10,
+            borderTopWidth: 2,
+            borderColor: 'white',
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            paddingHorizontal: 10,
+          }
+        },
+        'stylesheet.calendar.main': {
+          week: {
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            paddingHorizontal: 10,
+          },
+        },
+        'stylesheet.day.basic': {
+          base: {
+            width: 32,
+            height: 32,
+            alignItems: 'center',
+            justifyContent: 'center',
+            margin: 0,
+            padding: 0,
+          },
+        },
+
+      } as Theme}
+      style={{
+        borderRadius: 20,
+        borderWidth: 0,
+        borderColor: 'transparent',
+        width: 300,
+        paddingBottom: 10,
+
+      }}
+
+      // markingType={'period'}
+      renderArrow={direction => (
+        <Text className='text-white text-2xl'>{direction === 'left' ? '<' : '>'}</Text>
+      )}
+      renderHeader={date => (
+        <Text className='text-white font-bold text-2xl'>{date.toString('MMMM yyyy')}</Text>
+      )}
+    />
+  );
+};
