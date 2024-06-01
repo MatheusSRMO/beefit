cd29fc8c2561cb4aebd89bb56e3177c65e766455
MatheusSRMO <matheus.souzaribeiro@gmail.com>
2024-06-01 19:57:28 -0300

    feat: Add animated keyboard behavior to SignIn component
    The SignIn component in the `(tabs)/signIn.tsx` file has been updated to include animated keyboard behavior. This allows the component to respond to the keyboard visibility by animating the position of the background image and text elements. The purpose of this commit is to enhance the user experience and provide a smoother transition when the keyboard is shown or hidden.

--- src/app/(tabs)/signIn.tsx
+++ src/app/(tabs)/signIn.tsx
@@ -1,48 +1,89 @@
-import { View, Text, Image, TextInput, Pressable } from 'react-native';
+import { View, Keyboard, Animated, Easing } from 'react-native';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import Button from '@/components/button';
 import { useRouter } from 'expo-router';
+import TextInput from '@/components/textInput';
+import HelpButton from '@/components/helpButton';
 
-export default function signIn() {
+export default function SignIn() {
   const router = useRouter();
+
+  const [isKeyboardVisible, setKeyboardVisible] = React.useState(false);
 
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
 
+  const imageTop = React.useRef(new Animated.Value(-1)).current;
+  const textTop = React.useRef(new Animated.Value(70)).current;
+
+  React.useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      () => {
+        setKeyboardVisible(true);
+        Animated.timing(imageTop, {
+          toValue: -150,
+          duration: 200,
+          easing: Easing.out(Easing.ease),
+          useNativeDriver: false,
+        }).start();
+        Animated.timing(textTop, {
+          toValue: -100,
+          duration: 200,
+          easing: Easing.out(Easing.ease),
+          useNativeDriver: false,
+        }).start();
+      }
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      () => {
+        setKeyboardVisible(false);
+        Animated.timing(imageTop, {
+          toValue: -1,
+          duration: 200,
+          easing: Easing.out(Easing.ease),
+          useNativeDriver: false,
+        }).start();
+        Animated.timing(textTop, {
+          toValue: 70,
+          duration: 200,
+          easing: Easing.out(Easing.ease),
+          useNativeDriver: false,
+        }).start();
+      }
+    );
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
+
   return (
     <View className='w-full h-full flex items-center justify-center'>
-      <Image source={require('@/assets/images/loginBg.png')} className='absolute -top-1 left-0 w-full' style={{
+      <Animated.Image source={require('@/assets/images/loginBg.png')} className='absolute left-0 w-full' style={{
         resizeMode: 'stretch',
+        top: imageTop
       }} />
       <StatusBar style="light" backgroundColor='#192C64' />
 
-      <Text className='absolute top-24 text-[#FFDC98] text-4xl'>Oi Aluno </Text>
+      <Animated.Text className='absolute text-[#FFDC98] text-4xl' style={{
+        top: textTop
+      }}>Oi Aluno </Animated.Text>
 
-      <View className='w-full flex items-center justify-center gap-5'>
-        <TextInput
-          onChangeText={setUsername}
-          value={username}
-          placeholder='Usuário'
-          className='bg-[#21175C] text-[#FFDC98] text-xl px-10 py-3 w-9/12 rounded-3xl placeholder:text-[#FFDC98]'
-        />
+      <Animated.View className='w-full flex items-center justify-center gap-5' style={{ top: textTop }}>
+        <TextInput data={username} setData={setUsername} placeholder='Usuário' type='default' />
+        <TextInput data={password} setData={setPassword} placeholder='Senha' type='visible-password' />
+        
+        <Button title='Entrar' className='bg-[#528AA5] w-6/12 mt-10 rounded-full py-2' onPress={() => {
+          router.push('/main')
+        }} />
+      </Animated.View>
 
-        <TextInput
-          onChangeText={setPassword}
-          value={password}
-          placeholder='Senha'
-          className='bg-[#21175C] text-[#FFDC98] text-xl px-10 py-3 w-9/12 rounded-3xl placeholder:text-[#FFDC98]'
-          keyboardType='visible-password'
-        />
-      </View>
 
-      <Button title='Entrar' className='bg-[#528AA5] w-6/12 mt-10 rounded-full py-2' onPress={() => { }} />
-
-      <Pressable className='absolute bottom-10 z-30' onPress={() => {
-        router.push('/signUp')
-      }}>
-        <Text className='text-[#FFDC98]'>Dúvidas?</Text>
-      </Pressable>
+      <HelpButton title='Dúvidas?' onPress={() => {}} />
     </View>
-  )
+  );
 }
