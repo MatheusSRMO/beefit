2730ed1bbfb0932263bcfd1063257b25c5a46d17
MatheusSRMO <matheus.souzaribeiro@gmail.com>
2024-06-01 19:57:09 -0300

    feat: Refactor Home component and add HelpButton
    The Home component in the `(tabs)/index.tsx` file has been refactored to improve code readability and maintainability. Additionally, a new HelpButton component has been added to provide a convenient way to display a help text when pressed. This commit aims to enhance the Home component and introduce consistent styling and functionality throughout the application.

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,51 +1,35 @@
 import Button from '@/components/button';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react'
-import { Text, View, Image, Pressable } from 'react-native'
-import { Link, useRouter } from 'expo-router';
+import { View, Image, ImageBackground } from 'react-native'
+import { useRouter } from 'expo-router';
+import HelpButton from '@/components/helpButton';
 
 export default function Home() {
-    const router = useRouter();
+  const router = useRouter();
+  
+  return (
+    <ImageBackground source={require('@/assets/images/hive_background.png')} className='w-full h-full flex items-center justify-around'>
+      {/* muda a status bar */}
+      <StatusBar style="light" backgroundColor='#192C64' />
 
+      <Image
+        source={require("@/assets/images/beefit_logo.png")}
+        className='z-20 flex-1'
+        style={{ resizeMode: 'contain' }}
+      />
 
-    return (
+      <View className='w-full flex-1 items-center justify-center'>
+        <Button
+          title="Login"
+          className='bg-[#528AA5] w-9/12'
+          onPress={() => {
+            router.push('/signIn')
+          }}
+        />
+      </View>
 
-        <View className='w-full h-full flex items-center justify-around'>
-            {/* muda a status bar */}
-            <StatusBar style="light" backgroundColor='#192C64' />
-
-            {/* Adiciona uma imagem de fundo */}
-            <Image
-                source={require('@/assets/images/hive_background.png')}
-                className='w-full h-full absolute top-0 left-0 right-0 bottom-0 '
-                style={{ resizeMode: 'cover' }}
-            />
-
-            <Image
-                source={require("@/assets/images/beefit_logo.png")}
-                className='z-20' 
-            />
-
-
-            <View className='w-full flex items-center'>
-                <Button
-                    title="Login"
-                    className='bg-[#528AA5] w-9/12'
-                    onPress={() => {
-                        router.push('/signIn')
-                    }}
-                />
-            </View>
-
-            <Pressable className='absolute bottom-10' onPress={() => {
-                router.push('/sign-up')
-            }} >
-                <Text className='text-[#FFDC98]'>Ainda não é cadastrado?</Text>
-            </Pressable>
-        </View>
-
-
-
-
-    )
+      <HelpButton title='Ainda não é cadastrado?' onPress={() => { }} />
+    </ImageBackground>
+  )
 }
