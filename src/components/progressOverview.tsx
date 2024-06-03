c893d90fc36c10a6a21c8335e99acc9fbb17c468
MatheusSRMO <matheus.souzaribeiro@gmail.com>
2024-06-03 11:31:48 -0300

    refactor: Adjust top position of "Seus Rendimentos" text in ProgressOverview component

--- src/components/progressOverview.tsx
+++ src/components/progressOverview.tsx
@@ -9,7 +9,7 @@ interface ProgressOverviewProps {
 export default function ProgressOverview({ progress }: ProgressOverviewProps) {
   return (
     <View className='bg-[#080835] p-3 mt-10 w-[90%] rounded-full'>
-      <Text className="text-[#FFDC98] text-2xl font-bold -top-10">Seus Rendimentos</Text>
+      <Text className="text-[#FFDC98] text-2xl font-bold -top-8">Seus Rendimentos</Text>
 
       {/* ProgressBar */}
       <ProgressBar progress={progress} className='-top-4' />
