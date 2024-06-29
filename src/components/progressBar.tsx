0a1e67bc8ce2fc95a238cefede435a45e01e410d
Gabriella Sirtoli Borges <gabrielasirtoli@gmail.com>
2024-06-29 16:57:42 -0300

    fix: settings_page + progress bar

--- src/components/progressBar.tsx
+++ src/components/progressBar.tsx
@@ -1,14 +1,21 @@
 import { cn } from '@/lib/utils';
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 interface ProgressBarProps {
     progress: number; // Update the type to be a number
+    total: number;
     className?: string;
 }
 
-export default function ProgressBar({ progress, className }:ProgressBarProps) {
-  const width = progress * 100; // Converte progresso relativo para porcentagem
+export default function ProgressBar({ progress, total, className }: ProgressBarProps) {
+  let width = 0;
+  
+  if( total == 0 )  width = 0;
+  else if( total < progress )  width = 100;
+  else                    width = (progress / total) * 100;
+
+  console.log(progress, total)
 
   return (
     <View className={cn('h-3 w-[90%] bg-[#194064] rounded-full', className)}>
